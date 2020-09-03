aa3c38d9b6a62b17e8029e7615a675670846fe76
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-03 19:26:21 +0200

    Update Product.js

--- myApp/app/Model/Product.js
+++ myApp/app/Model/Product.js
@@ -16,6 +16,18 @@ class Product extends Lucid {
   childern(){
   	return this.hasMany("App/Model/Product","parent_id","id");
   }
+  filter(filters={},used_all=false){
+    return (yield(Product.all().toJson())).
+    filter({extrainfo}=>{
+      if(used_all==true)
+        return filters==extrainfo
+      else
+        {
+          obj_keys=Object.keys(filters).filter(k=>filters[k]!=null||filters[k]!=undefined)
+          return obj_key.map(key=>filters[key]==extrainfo[key])
+        }
+    })
+  }
 }
 
 module.exports = Product
