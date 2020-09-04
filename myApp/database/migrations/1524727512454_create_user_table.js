6c6e12ac3c4cf7adf0bd22d56e0ef4905a88a465
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-04 04:33:44 +0200

    Update 1524727512454_create_user_table.js

--- myApp/database/migrations/1524727512454_create_user_table.js
+++ myApp/database/migrations/1524727512454_create_user_table.js
@@ -16,7 +16,7 @@ class UsersTableSchema extends Schema {
       table.string("Password").nullable(false)
       table.string("Email",50).unique()
       table.string("type").nullable(false)
-      table.integer("related_to_id").unsigned()
+      table.integer("related_to_id").unsigned().default(0)
       table.foreign("related_to_id").references("id").on("users")
       table.softDeletes()
     })
