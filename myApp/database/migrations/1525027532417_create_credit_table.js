64798472de26d24e1031c821214f2030850f85ac
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-06 19:13:50 +0200

    Update 1525027532417_create_credit_table.js

--- myApp/database/migrations/1525027532417_create_credit_table.js
+++ myApp/database/migrations/1525027532417_create_credit_table.js
@@ -10,13 +10,13 @@ class CreditsTableSchema extends Schema {
 
   up () {
     this.create('credits', (table) => {
-      table.increments()
+      table.increments("cid")
       table.timestamps()
       table.text("creditNo")
       table.date("expires")
       table.integer("cvv")
       table.integer("uid").unsigned()
-      table.foreign("uid").references("id").on("users")
+      table.foreign("uid").references("uid").on("users")
       table.integer("balance").unsigned()
     })
   }
