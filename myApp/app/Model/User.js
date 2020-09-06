56c5ea66305c5e01b6812208170cc95bfdf8b507
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-06 19:12:09 +0200

    Update User.js

--- myApp/app/Model/User.js
+++ myApp/app/Model/User.js
@@ -8,16 +8,16 @@ class User extends Lucid {
     return 'mysql'
   }
   shops(){
-    return this.hasMany("App/Model/Shop","uid","id");
+    return this.hasMany("App/Model/Shop","uid","uid");
   }
   type(){
    return this.belongsTo("App/Model/UserType","related_type_id","type_id")
   }
   wallet(){
-    return this.hasOne("App/Model/Wallet","id","wid")
+    return this.hasOne("App/Model/Wallet","wid","wid")
   }
   orders(type="order"){
-   return this.hasMany("App/Model/Order","id","uid").where({type:type})
+   return this.hasMany("App/Model/Order","uid","uid").where({type:type})
   }
   
 
