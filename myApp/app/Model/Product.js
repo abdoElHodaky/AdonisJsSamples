8fed7b5e7809a0f33c63fd11b08ab13309e701db
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-06 23:53:40 +0200

    Update Product.js

--- myApp/app/Model/Product.js
+++ myApp/app/Model/Product.js
@@ -16,8 +16,8 @@ class Product extends Lucid {
   childern(){
   	return this.hasMany("App/Model/Product","parent_id","pid");
   }
-  static get filter(filters={},use_all=false,used=false){
-    return (yield(Product.query().where('used',used).with("specs").fetch().toJSON())).
+  static get filter(filters=[],use_all=false,used=false){
+    /*return yield(Product.query().where('used',used).with("specs").fetch()).
     filter({specs}=>{
       if(used_all==true)
         return filters==specs
@@ -26,7 +26,14 @@ class Product extends Lucid {
           obj_keys=Object.keys(filters).filter(k=>filters[k]!=null||filters[k]!=undefined)
           return obj_key.map(key=>filters[key]==specs[key])
         }
+    })*/
+    var products=yield Product.query(builder=>builder.where("used",used)).fetch();
+    return products.filter(p=>{
+      return yield (p.specs()).find((v,i)=>(v.name==filters[i].name&&v.value==filters[¡].value))
     })
+  }
+  versions(){
+   return this.hasMany("App/Model/ProductVersion","pid","pid");
   }
 }
 
