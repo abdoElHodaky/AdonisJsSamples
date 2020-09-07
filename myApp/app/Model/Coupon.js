92537f5e8aa9b505fe9d4c4328843814d361cd41
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-07 18:06:59 +0200

    Update Coupon.js

--- myApp/app/Model/Coupon.js
+++ myApp/app/Model/Coupon.js
@@ -1,1 +1,14 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Coupon extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+  static get On(){}
+}
+
+module.exports = Coupon
