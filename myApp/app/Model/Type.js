058cc16efd97bd61fbe20248ebcf5d81349ee0d5
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-07 18:56:12 +0200

    Update Type.js

--- myApp/app/Model/Type.js
+++ myApp/app/Model/Type.js
@@ -1,1 +1,16 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Type extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+  users(){
+   this.hasMany("App/Model/User","type_id","related_type_id")
+  }
+}
+
+module.exports = Type
