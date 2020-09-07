e1f52a6b9c644af1991f2504f3fa25d09d631360
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-07 14:57:47 +0200

    Create Offer.js

---
+++ myApp/app/Model/Offer.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Offer extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+   attachments(){
+    return this.hasManyThrough("App/Model/OfferAttachment","attachments","aid","aid")
+   }
+   user(){
+    return this.belongsTo("App/Model/User","uid","uid")
+  }
+  
+}
+
+module.exports = Offer
