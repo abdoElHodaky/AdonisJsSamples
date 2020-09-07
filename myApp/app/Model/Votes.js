3d1db88cefb773b8c67f48177f2fbcb86028d693
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-07 17:35:55 +0200

    Update Votes.js

--- myApp/app/Model/Votes.js
+++ myApp/app/Model/Votes.js
@@ -5,18 +5,6 @@ const Lucid = use('Lucid')
 
 class Vote extends Lucid {
 
-  static get connection () {
-    return 'mysql'
-  }
-  shop(){
-  	return this.belongsTo("App/Model/Shop",'OnId','sid');
-  }
-  products(){
-   return return this.belongsTo("App/Models/Product","OnId","pid"'use strict'
-
-const Lucid = use('Lucid')
-
-class Cat extends Lucid {
 
   static get connection () {
     return 'mysql'
