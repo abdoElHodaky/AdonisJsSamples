2bef363126cee7e47feeb47b944bb3e956b46540
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-07 17:35:11 +0200

    Update Votes.js

--- myApp/app/Model/Votes.js
+++ myApp/app/Model/Votes.js
@@ -1,1 +1,35 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Vote extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+  shop(){
+  	return this.belongsTo("App/Model/Shop",'OnId','sid');
+  }
+  products(){
+   return return this.belongsTo("App/Models/Product","OnId","pid"'use strict'
+
+const Lucid = use('Lucid')
+
+class Cat extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+  shop(){
+  	return this.belongsTo("App/Model/Shop",'OnId','sid').where("OnType","shpos");
+  }
+  products(){
+      return return this.belongsTo("App/Models/Product","OnId,"pid").where("OnType","products");
+  }
+    users(){
+      return return this.belongsTo("App/Models/User","OnId,"uid").where("OnType","users");
+  }
+}
+
+module.exports = Vote
