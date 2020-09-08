9a56b92b206b428b69b13259509464741ed0a1eb
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 22:12:15 +0200

    Update 1525506335000_create_events_table.js

--- myApp/database/migrations/1525506335000_create_events_table.js
+++ myApp/database/migrations/1525506335000_create_events_table.js
@@ -1,1 +1,31 @@
 
+
+'use strict'
+
+const Schema = use('Schema')
+
+class EventTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('events', (table) => {
+      table.increments("evtid")
+      table.timestamps()
+      table.string("Name",50).unique()
+      table.json("addit_info").default("{}")
+      table.integer("related_to_id").unsigned()
+      table.foreign("related_to_id").references("evtid").on("events")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('events')
+  }
+
+}
+
+module.exports = EventTableSchema
