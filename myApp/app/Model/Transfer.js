f10ce0cdbb9e809000a4c4a348460a8b884b963b
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 11:21:58 +0200

    Update Transfer.js

--- myApp/app/Model/Transfer.js
+++ myApp/app/Model/Transfer.js
@@ -1,1 +1,22 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Transfer extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+  order(){
+    return this.belongsTo("App/Model/Order",'oid','oid');
+  }
+  sender(){
+     return this.belongsTo("App/Models/User","uid","uid")
+  }
+  receiver(){
+     return this.belongsTo("App/Models/User","uid","uid")
+  }
+}
+
+module.exports = Transfer
