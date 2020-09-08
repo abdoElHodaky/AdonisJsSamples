e8e8e0d123d633eb22c3b790a1b799060cc74fda
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 15:01:00 +0200

    Update 1525505160000_create_comments_table.js

--- myApp/database/migrations/1525505160000_create_comments_table.js
+++ myApp/database/migrations/1525505160000_create_comments_table.js
@@ -1,1 +1,31 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class CommentsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('comments', (table) => {
+      table.increments("comid")
+      table.timestamps()
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.string("status").nullable()
+      table.string("type").nullable()
+      table.integer("comattid").unsigned()
+      table.foreign("comattid").references("comattid").on("comments_attachment")
+    
+  }
+
+  down () {
+    this.drop('comments')
+  }
+
+}
+
+module.exports = AttachmentsTableSchema
