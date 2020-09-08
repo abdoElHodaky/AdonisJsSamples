017ed9f745996e19e53c29d6700dd4f16fb865f7
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 17:46:06 +0200

    Create ProductVotes.js

---
+++ myApp/app/Model/ProductVotes.js
@@ -0,0 +1,20 @@
+
+'use strict'
+
+const Lucid = use('Lucid')
+
+class ProductVotes extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+   votes(){
+    return this.hasMany("App/Model/Votes","votid","votid")
+   }
+   product(){
+    return this.belongsTo("App/Model/Product","pid","pid")
+  }
+  
+}
+
+module.exports = ProductVotes
