9b66402ba58a99f05f0f5ab874a1b914c7636f0f
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 12:12:54 +0200

    Update 1525505040000_create_offers_table.js

--- myApp/database/migrations/1525505040000_create_offers_table.js
+++ myApp/database/migrations/1525505040000_create_offers_table.js
@@ -1,1 +1,28 @@
+'use strict'
 
+const Schema = use('Schema')
+
+class OffersTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('offers', (table) => {
+      table.increments()
+      table.timestamps()
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("id").on("users")
+      table.string("status").nullable()
+
+    })
+  }
+
+  down () {
+    this.drop('offers')
+  }
+
+}
+
+module.exports = OffersTableSchema
