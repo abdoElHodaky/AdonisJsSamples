37b8c90542cccd220c860b7be2887168d2200b5e
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 21:41:11 +0200

    Update 1525506035000_create_types_shops_table.js

--- myApp/database/migrations/1525506035000_create_types_shops_table.js
+++ myApp/database/migrations/1525506035000_create_types_shops_table.js
@@ -1,1 +1,29 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class TypesShopsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('types_shops', (table) => {
+      table.increments("shptype_id")
+      table.timestamps()
+      table.string("Name",50).unique()
+      table.integer("related_to_id").unsigned()
+      table.foreign("related_to_id").references("shptype_id").on("types_shops")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('types_shops')
+  }
+
+}
+
+module.exports = TypesShopsTableSchema
