83f87d523b2971ae6a95b3de119ec20382b66025
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 19:03:00 +0200

    Update 1525505670000_create_visits_table.js

--- myApp/database/migrations/1525505670000_create_visits_table.js
+++ myApp/database/migrations/1525505670000_create_visits_table.js
@@ -1,1 +1,31 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class VisitsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('visits', (table) => {
+      table.increments("visid")
+      table.timestamps()
+      table.string("scope",50)/*page.[name]*/
+      table.boolean("isGuest").default(true)
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.integer("session_id")
+      table.foreign("session_id").references("session_id").on("sessions"
+     
+  }
+
+  down () {
+    this.drop('visits')
+  }
+
+}
+
+module.exports = VisitsTableSchema
