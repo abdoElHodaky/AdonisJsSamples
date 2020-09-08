8fbcb0212dc2bc142e95bb8049ddd736fd855651
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 18:32:22 +0200

    Update 1525505537000_create_ads_attachments_table.js

--- myApp/database/migrations/1525505537000_create_ads_attachments_table.js
+++ myApp/database/migrations/1525505537000_create_ads_attachments_table.js
@@ -1,1 +1,29 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class AdsAttachmentsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('ads_attachments', (table) => {
+      table.increments("adaid")
+      table.timestamps()
+      table.integer("aid").unsigned()
+      table.foreign("aid").references("aid").on("attachments")
+      table.integer("adid").unsigned()
+      table.foreign("adid").references("adid").on("ads")
+     
+  }
+
+  down () {
+    this.drop('ads_attachments')
+  }
+
+}
+
+module.exports = AdsAttachmentsTableSchema
