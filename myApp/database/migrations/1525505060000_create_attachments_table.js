0c031d44d0cb9c9e2c137454603d1d19658777e7
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 12:28:29 +0200

    Update 1525505060000_create_attachments_table.js

--- myApp/database/migrations/1525505060000_create_attachments_table.js
+++ myApp/database/migrations/1525505060000_create_attachments_table.js
@@ -1,1 +1,30 @@
+'use strict'
 
+const Schema = use('Schema')
+
+class AttachmentsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('attachments', (table) => {
+      table.increments("attid")
+      table.timestamps()
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.string("status").nullable()
+      table.string("type").nullable()
+      table.enum("attachment_type",["file","url","thumbnail"])
+      table.binary(attachment_src)
+      })
+  }
+
+  down () {
+    this.drop('attachments')
+  }
+
+}
+
+module.exports = AttachmentsTableSchema
