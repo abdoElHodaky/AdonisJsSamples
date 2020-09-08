2220a3cf7638608551d852bb4a5020ea9395875a
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 17:34:00 +0200

    Update 1525505347000_create_votes_table.js

--- myApp/database/migrations/1525505347000_create_votes_table.js
+++ myApp/database/migrations/1525505347000_create_votes_table.js
@@ -1,1 +1,28 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class VotesTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('votes', (table) => {
+      table.increments("votid")
+      table.timestamps()
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      
+     
+  }
+
+  down () {
+    this.drop('votes')
+  }
+
+}
+
+module.exports = VotesTableSchema
 
