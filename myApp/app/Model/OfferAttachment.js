a1252a2e0be621fd0e5b0e5fdfad1712c1915e19
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-08 16:28:44 +0200

    Update OfferAttachment.js

--- myApp/app/Model/OfferAttachment.js
+++ myApp/app/Model/OfferAttachment.js
@@ -1,1 +1,20 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class OfferAttachment extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+   attachments(){
+    return this.hasMany("App/Model/Attachment","aid","aid")
+   }
+   offers(){
+    return this.belongsTo("App/Model/Offer","offid","offid")
+  }
+  
+}
+
+module.exports = OfferAttachment
