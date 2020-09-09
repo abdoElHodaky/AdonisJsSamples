93a482dbd5d8d56fe2978bea5171fa890a957451
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-09 14:49:49 +0200

    Update 1525507157000_create_activities_table.js

--- myApp/database/migrations/1525507157000_create_activities_table.js
+++ myApp/database/migrations/1525507157000_create_activities_table.js
@@ -1,1 +1,30 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class ActivitiessTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('activities', (table) => {
+      table.increments("actid")
+      table.timestamps()
+      table.json("action")
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('activities')
+  }
+
+}
+
+module.exports = ActivitiessTableSchema
+
