6e0f3c09997b684c0a9e7c26ac8d8f2ede8c90a9
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-09 11:00:49 +0200

    Update 1525506645000_create_shops_followers_table.js

--- myApp/database/migrations/1525506645000_create_shops_followers_table.js
+++ myApp/database/migrations/1525506645000_create_shops_followers_table.js
@@ -1,1 +1,30 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class ShopsFollowersTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('shops_followers', (table) => {
+      table.increments("follsid")
+      table.timestamps()
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.integer("sid").unsigned()
+      table.foreign("sid").references("sid").on("sid")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('shops')
+  }
+
+}
+
+module.exports = ShopsFollowersTableSchema
