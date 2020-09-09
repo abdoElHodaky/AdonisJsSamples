10a315c021259b98295116b192dd2eabc1aa871d
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-09 11:06:16 +0200

    Update ShopFollower.js

--- myApp/app/Model/ShopFollower.js
+++ myApp/app/Model/ShopFollower.js
@@ -1,1 +1,19 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class ShopFollower extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  followers(){
+   return this.belongsTo("App/Model/Shop","sid","sid");
+  }
+  
+
+}
+
+module.exports = ShopFollower
