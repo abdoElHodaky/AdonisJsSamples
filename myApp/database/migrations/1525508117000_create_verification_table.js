1cbe225f4599e7be052ef88e75ac7523952bb4f2
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-10 21:04:30 +0200

    Update 1525508117000_create_verification_table.js

--- myApp/database/migrations/1525508117000_create_verification_table.js
+++ myApp/database/migrations/1525508117000_create_verification_table.js
@@ -3,29 +3,31 @@
 
 const Schema = use('Schema')
 
-class VerificationTableSchema extends Schema {
+class VerificationsTableSchema extends Schema {
 
   static get connection () {
     return 'mysql'
   }
 
   up () {
-    this.create('verification', (table) => {
+    this.create('verifications', (table) => {
       table.increments("verifid")
       table.timestamps()
       table.integer("uid").unsigned()
       table.foreign("uid").references("uid").on("users")
       table.enu("type",["device","email"])
-      table.integer("devid").unsigned()
-      table.foreign("devid").references("devid").on("devices")
+      /*table.integer("devid").unsigned()
+      table.foreign("devid").references("devid").on("devices")*/
+      table.integer("related_to_id").unsigned()
+      table.foreign("related_to_id").references("related_to_id").on("verifications")
       table.softDeletes()
     })
   }
 
   down () {
-    this.drop('verification')
+    this.drop('verifications')
   }
 
 }
 
-module.exports = VerificationTableSchema
+module.exports = VerificationsTableSchema
