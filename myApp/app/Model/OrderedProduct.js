673cad3600f2e2cb4a2accc2ad0d60aef7105a95
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-11 14:54:53 +0200

    Update OrderedProduct.js

--- myApp/app/Model/OrderedProduct.js
+++ myApp/app/Model/OrderedProduct.js
@@ -11,7 +11,7 @@ class OrderedProduct extends Lucid {
   	return this.belongsTo("App/Model/Product",'id','pid');
   }
   coupon(){
-    return this.belongsMany("App/Model/Coupon",'opid','coupid',"coupid","opid");
+    return this.belongsMany("App/Model/Coupon","coupid","opid","opid","coupid");
   }
 }
 
