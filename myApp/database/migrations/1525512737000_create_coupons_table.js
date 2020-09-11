5a6d14066adcbdf26cc9c945bb2d9b36d2d628cc
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-11 12:08:12 +0200

    Update 1525512737000_create_coupons_table.js

--- myApp/database/migrations/1525512737000_create_coupons_table.js
+++ myApp/database/migrations/1525512737000_create_coupons_table.js
@@ -1,1 +1,33 @@
 
+'use strict'
+
+const Schema = use('Schema')
+
+class CouponsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('coupons', (table) => {
+      table.increments("coupid")
+      table.timestamps()
+      table.string("type",30).default("notMarket")
+      table.string("Code",50).unique()
+      table.float("amount")
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.timestamp("expired_at")
+      table.boolean("expired").default(false)
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('coupons')
+  }
+
+}
+
+module.exports = CouponsTableSchema
