656083e8fce318a9d5efd6d8bcf42a039a561ade
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-11 16:34:20 +0200

    Update 1525505160000_create_comments_table.js

--- myApp/database/migrations/1525505160000_create_comments_table.js
+++ myApp/database/migrations/1525505160000_create_comments_table.js
@@ -15,11 +15,13 @@ class CommentsTableSchema extends Schema {
       table.timestamps()
       table.integer("uid").unsigned()
       table.foreign("uid").references("uid").on("users")
+      table.integer("parent_id").unsigned().default(0)
+      table.foreign("parent_id").references("parent_id").on("comments")
       table.string("status").nullable()
       table.string("type").nullable()
       table.integer("comattid").unsigned()
       table.foreign("comattid").references("comattid").on("comments_attachments")
-       
+      table.softDeletes()
 
   }
 
