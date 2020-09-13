4785dea25cb5b2294e94f50484dc41c8c0451a63
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-13 11:31:21 +0200

    Update Event.js

--- myApp/app/Model/Event.js
+++ myApp/app/Model/Event.js
@@ -1,1 +1,23 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Event extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+  user(){
+  	return this.belongsTo("App/Models/User",'uid','uid');
+  }
+  
+  users(){
+   return this.belongsMany("App/Model/User"
+   ,"evtid","uid","uid","evtid")
+    .pivotTable("events_users")
+  }
+  
+}
+
+module.exports = Event
