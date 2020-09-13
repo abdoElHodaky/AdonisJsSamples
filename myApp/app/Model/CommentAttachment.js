eaab0c8e0543e3be18f55d6c7d78076b31a19f6a
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-13 23:05:35 +0200

    Update CommentAttachment.js

--- myApp/app/Model/CommentAttachment.js
+++ myApp/app/Model/CommentAttachment.js
@@ -8,8 +8,8 @@ class CommentAttachment extends Lucid {
     return 'mysql'
   }
    attachments(){
-    //return this.hasMany("App/Model/Attachment","aid","aid")
-     return this.belongsTo("App/Model/Attachment","aid","aid")
+    return this.hasMany("App/Model/Attachment","aid","aid")
+     //return this.belongsTo("App/Model/Attachment","aid","aid")
    
    }
   
