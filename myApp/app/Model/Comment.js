c93bd528ea00f7bf7470a3ad6573efeeb1eeea67
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-13 22:58:41 +0200

    Update Comment.js

--- myApp/app/Model/Comment.js
+++ myApp/app/Model/Comment.js
@@ -13,7 +13,9 @@ class Comment extends Lucid {
      return this.belongsMany("App/Model/User","commid","uid","uid","commid")
   }
   attachments(){
-    return this.manyThrough("App/Model/CommentAttachment","attachments","commaid","aid")
+    //return this.manyThrough("App/Model/CommentAttachment","attachments","commaid","aid")
+     return this.belongsMany("aid","commid","commid","aid").pivotModel("App/Model/CommentAttachment")
+   
    }
   comments(){
    return this.hasMany("App/Model/Comment","commid","parent_id")
