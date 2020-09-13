c733ba28da56019a06f944ba8a1ec6c6dc8569ea
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-13 17:55:57 +0200

    Create 1525522217000_create_affiliates_products_table.js

---
+++ myApp/database/migrations/1525522217000_create_affiliates_products_table.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class AffiliatesProductsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('affiliates_products', (table) => {
+      table.increments("affilpid")
+      table.timestamps()
+      //table.string("affiliate_code",30)
+      table.integer("pid").unsigned().default(0)
+      table.foreign("pid").references("pid").on("products")
+      table.integer("uid").unsigned().default(0)
+      table.foreign("uid").references("uid").on("users")
+      table.integer("affilid").unsigned().default(0)
+      table.foreign("affilid").references("affilid").on("affiliates")
+      //table.timestamp("expired_at")
+      table.softDeletes()
+  }
+
+  down () {
+    this.drop('affiliates_products')
+  }
+
+}
+
+module.exports = AffiliatesProductsTableSchema
