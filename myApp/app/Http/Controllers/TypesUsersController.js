dcd4c3a11f95835d47fd69698d1650e58592d8f5
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-15 19:51:25 +0200

    Create TypesUsersController.js

---
+++ myApp/app/Http/Controllers/TypesUsersController.js
@@ -0,0 +1,45 @@
+'use strict'
+const TypesUsers=use("App/Model/UserType")
+class TypesUsersController {
+    
+  * index(request, response) {
+   /* if (request.session.get("user")) {
+      yield response.sendView("Cart",{})
+    } else {
+      response.redirect("Home")
+    }
+     */
+    return response.json(yield TypesUsers.all())
+  }
+
+  * create(request, response) {
+    //
+    
+  }
+
+  * store(request, response) {
+    //
+      response.json( TypesUsers.create(yield request.post()))
+  }
+
+  * show(request, response) {
+     return reaponse.json(yield TypesUsers.find(request.params().id)
+    .load("users"))
+    //
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+}
+
+module.exports = TypesUsersController
