92708b51c5ac357ce96f5a3e7a31d9cd7c8ac5d1
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-16 12:32:26 +0200

    Update ProductSpecController.js

--- myApp/app/Http/Controllers/ProductSpecController.js
+++ myApp/app/Http/Controllers/ProductSpecController.js
@@ -14,7 +14,7 @@ class ProductSpecController {
   * store(request, response) {
     //
     var inputs=request.post()
-    var specs=yield Product.find(request.params().pid).specs().create({
+    var specs=yield Product.find(request.params().pid).specs().findOrCreate(request.get().specid,{
       Name:inputs.Name,
       Value:input.value
     });
@@ -26,7 +26,7 @@ class ProductSpecController {
   * show(request, response) {
     //
      var product=Product.find(request.params().id)
-     response.json(product.loadMany((["specs","specs.childern","comments","offers","attachments","children"]))
+     response.json(product.loadMany((["specs","specs.childern","comments"]))
   }
 
   * edit(request, response) {
@@ -35,6 +35,10 @@ class ProductSpecController {
 
   * update(request, response) {
     //
+      var product=Product.find(request.params().pid)
+      response.json(product.specs()
+     .where({"specid":request.params().specid}).
+     update(request.post()))
   }
 
   * destroy(request, response) {
