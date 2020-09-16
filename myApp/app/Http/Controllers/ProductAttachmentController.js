34e8ac90ad86673b23154dd0c54b7447713aa558
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-16 19:29:30 +0200

    Update ProductAttachmentController.js

--- myApp/app/Http/Controllers/ProductAttachmentController.js
+++ myApp/app/Http/Controllers/ProductAttachmentController.js
@@ -17,7 +17,7 @@ class ProductAttachmentController {
     var inputs=request.post()
     var product=yield Product.find(request.params().pid),
     response.json(yield product.attachments().
-    attach([(yield Attachment.create(inputs)).aid]))
+    attach([(yield (user.attachments().create(inputs)).aid]))
     
     
   }
@@ -25,8 +25,9 @@ class ProductAttachmentController {
   * show(request, response) {
     //
      var product=Product.findOrFail(request.params().id)
-     response.json(product.loadMany((["specs","specs.childern","comments"]))
-  }
+     response.json(product.attachments().
+     findOrFail(request.params().aid))
+   }
 
   * edit(request, response) {
     //
@@ -34,10 +35,7 @@ class ProductAttachmentController {
 
   * update(request, response) {
     //
-      var product=Product.find(request.params().pid)
-      response.json(product.specs()
-     .where({"specid":request.params().specid}).
-     update(request.post()))
+      
   }
 
   * destroy(request, response) {
