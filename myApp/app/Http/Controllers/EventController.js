4a5dbd1e0eec7962fa92fc033a02e0fbf82aa175
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-16 15:04:44 +0200

    Update EventController.js

--- myApp/app/Http/Controllers/EventController.js
+++ myApp/app/Http/Controllers/EventController.js
@@ -1,1 +1,60 @@
 
+'use strict'
+//const User=use("App/Model/User")
+class EventController {
+
+  * index(request, response) {
+   /* if (request.session.get("user")) {
+      yield response.sendView("Cart",{})
+    } else {
+      response.redirect("Home")
+    }
+     */
+     response.json(Event.all())
+  }
+
+  * create(request, response) {
+    //
+    var inputs=request.post();
+    order.create(inputs);
+  }
+
+  * store(request, response) {
+    //
+    var user=yield request.auth.getUser()
+    ,order=user.orders().create()
+    yield order.products().createMany(request.post())
+    response.json(order.load("products"))
+
+ }
+
+  * show(request, response) {
+    //
+     var user=yield request.auth.getUser()
+    ,order=user.orders().find(request.params().id)
+    response.json (yield order.load("products"))
+
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+     var user=yield request.auth.getUser(),inputs=request.post()
+    ,order=user.orders().find(request.params().id)
+     response.json( 
+      order.products().where({"opid":inputs.opid}).
+      update(inputs.product)
+    )
+
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+}
+
+module.exports = EventController
