1b552bfc43951e9bf06ce4a8f08eb338194fed78
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-16 21:03:26 +0200

    Update HomeController.js

--- myApp/app/Http/Controllers/HomeController.js
+++ myApp/app/Http/Controllers/HomeController.js
@@ -1,1 +1,43 @@
+'use strict'
+//const User=use("App/Model/User"),Client=use("App/Model/Client");
+class HomeController {
 
+  * index(request, response) {
+    //
+    
+  }
+
+  * create(request, response) {
+    //
+
+  }
+
+  * store(request, response) {
+    //
+    
+  }
+
+  * show(request, response) {
+    //
+    
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+  
+  
+   
+
+}
+
+module.exports = HomeController
