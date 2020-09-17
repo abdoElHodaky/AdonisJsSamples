0622dc255b4c42cd04845e28927fcca6c1326306
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-17 12:07:48 +0200

    Create AffilitesController.js

---
+++ myApp/app/Http/Controllers/AffilitesController.js
@@ -0,0 +1,41 @@
+'use strict'
+const Affiliate=use("App/Model/Affiliate");
+class AffilitesController {
+
+  * index(request, response) {
+    //
+    
+  }
+
+  * create(request, response) {
+    //
+
+  }
+
+  * store(request, response) {
+    //
+    var inputs=yield request.post();
+    ;
+  }
+
+  * show(request, response) {
+    //
+      }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+ 
+
+}
+
+module.exports = AffilitesController
