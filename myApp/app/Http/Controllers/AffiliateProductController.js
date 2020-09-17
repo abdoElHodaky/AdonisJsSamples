fc696f9848c65c6762125233c1ab3d4e80f5c430
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-17 12:58:38 +0200

    Create AffiliateProductController.js

---
+++ myApp/app/Http/Controllers/AffiliateProductController.js
@@ -0,0 +1,44 @@
+'use strict'
+const Affiliate=use("App/Model/Affiliate"),
+const Product=use("App/Model/Product")
+class AffiliteProductController {
+
+  * index(request, response) {
+    //
+    //response.json(yield Affiliate.query().with("products").fetch())
+  }
+
+  * create(request, response) {
+    //
+
+  }
+
+  * store(request, response) {
+    //
+    var inputs=yield request.post(),
+    affiliate=yield Affiliate.findOrFail(request.params().affilid)
+    affiliate.products().attach(inputs.affiliate_products)
+    response.json(affiliate.load("products"))
+  }
+
+  * show(request, response) {
+    //
+      }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+ 
+
+}
+
+module.exports = AffiliteProductController
