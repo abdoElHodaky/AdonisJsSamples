e6fc6d07c5c7056816627b3040fdf714cf69851d
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-18 11:43:45 +0200

    Update MessageController.js

--- myApp/app/Http/Controllers/MessageController.js
+++ myApp/app/Http/Controllers/MessageController.js
@@ -1,1 +1,61 @@
 
+'use strict'
+//const Order=use("App/Model/Order")
+class MessageController {
+
+  * index(request, response) {
+    var user=yield request.auth.getUser()
+    response.json(yield user.conversations().messages())
+     
+  }
+
+  * create(request, response) {
+    //
+    
+  }
+
+  * store(request, response) {
+    //
+    var user=yield request.auth.getUser(),
+    inputs=request.post(),
+    message=user.conversations()
+    .findOrFail(request.params().cmessgid)
+     messages().create({
+     type:"message",
+     receiver_id:inputs.receiver,
+     content:inputs.message
+    })
+  
+    response.json(message)
+
+ }
+
+  * show(request, response) {
+    //
+     
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+     var user=yield request.auth.getUser(),
+    inputs=request.post(),
+    message=user.conversations()
+    .findOrFail(request.params().cmessgid)
+     messages().where({messgid:request.params().messgid}).update
+     content:inputs.message
+    })
+     response.json(message)
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+}
+
+module.exports = MessageController
+
