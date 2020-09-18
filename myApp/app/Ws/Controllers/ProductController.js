59b74c468895f9974cece726ae415d2be341e504
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-18 12:38:16 +0200

    Update ProductController.js

--- myApp/app/Ws/Controllers/ProductController.js
+++ myApp/app/Ws/Controllers/ProductController.js
@@ -1,1 +1,17 @@
+'use strict'
+const Oproduct=use("App/Model/OrderedProduct"),
+product=use("App/Model/Product"),
+class ProductController {
+
+  constructor (socket, request) {
+    this.socket = socket
+    this.request = request
+    this.user=yield request.auth.getUser()
+  }
+  
+  
+  
+}
+
+module.exports = ProductController
 
