2086dff77390ef8d844a7a1f25812fb6ab05b697
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-18 11:33:52 +0200

    Update ConversationController.js

--- myApp/app/Http/Controllers/ConversationController.js
+++ myApp/app/Http/Controllers/ConversationController.js
@@ -1,1 +1,53 @@
+'use strict'
+//const Order=use("App/Model/Order")
+class ConventionController {
+
+  * index(request, response) {
+    //response.json()
+     
+  }
+
+  * create(request, response) {
+    //
+    
+  }
+
+  * store(request, response) {
+    //
+    var user=yield request.auth.getUser(),
+    inputs=request.post(),
+    conversation=user.messages().create({
+     type:"conversation",
+     receiver_id:inputs.receiver
+    })
+  //  yield order.products(inputs.transfer).createMany(inputs.products)
+    response.json(conversation)
+
+ }
+
+  * show(request, response) {
+    //
+     var user=yield request.auth.getUser()
+    ,conversation=user.messages().find(request.params().id)
+    response.json (yield conversation.load("messages"))
+
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+     
+
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+}
+
+module.exports = ConventionController
 
