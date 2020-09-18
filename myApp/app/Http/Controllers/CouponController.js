3f81fc2f57d0875b0248aaa175e43d86d2f3ec73
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-18 11:16:26 +0200

    Update CouponController.js

--- myApp/app/Http/Controllers/CouponController.js
+++ myApp/app/Http/Controllers/CouponController.js
@@ -1,1 +1,49 @@
 
+'use strict'
+//const Order=use("App/Model/Order")
+class CouponController {
+
+  * index(request, response) {
+    //response.json()
+     
+  }
+
+  * create(request, response) {
+    //
+    
+  }
+
+  * store(request, response) {
+    //
+    var user=yield request.auth.getUser(),
+    inputs=request.post(),
+    coupon=user.coupons().create(inputs)
+    response.json(coupon)
+
+ }
+
+  * show(request, response) {
+    //
+     var user=yield request.auth.getUser()
+    ,order=user.orders().find(request.params().id)
+    response.json (yield order.load("products("+request.post().transfer+")"))
+
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+     
+
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+}
+
+module.exports = CouponController
