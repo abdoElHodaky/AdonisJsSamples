171d3ae1d048178b62c2b39c0b6579269c406ba6
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-18 11:51:21 +0200

    Update MessageAttachmentController.js

--- myApp/app/Http/Controllers/MessageAttachmentController.js
+++ myApp/app/Http/Controllers/MessageAttachmentController.js
@@ -1,1 +1,46 @@
 
+'use strict'
+const Message=use("App/Model/Message");
+class MessageAttachmentController {
+
+  * index(request, response) {
+    var conversation=Message.findBy({type:"conversation",messgid:request.params().cmessgid})
+    response.json(yield conversation.load("attachments"))
+  }
+
+  * create(request, response) {
+    //
+  }
+
+  * store(request, response) {
+    //
+    var inputs=request.post(),
+    cmessgid=request.params().cmessgid,
+    conversation=yield Message.findBy({type:"conversation",
+    messgid:cmessgid}),
+    attachment=yield Attachment.create(inputs.attachment)
+    conversation.attachments().attach([attachment.aid])
+    response.json(conversation.load("attachments"))
+   }
+  * show(request, response) {
+    //
+    }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+     
+  }
+
+  * destroy(request, response) {
+    //
+  }
+  
+
+
+}
+
+module.exports = MessageAttachmentController
