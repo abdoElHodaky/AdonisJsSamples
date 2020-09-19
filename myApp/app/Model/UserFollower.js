5dd0467b51fb1144a6170b8bfcc15f3c28344821
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-19 13:02:27 +0200

    Update UserFollower.js

--- myApp/app/Model/UserFollower.js
+++ myApp/app/Model/UserFollower.js
@@ -1,1 +1,34 @@
 
+'use strict'
+
+const Lucid = use('Lucid'),
+Activity=use("App/Model/Activity")
+
+class UserFollower extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+   static boot(){
+    super.boot()
+    this.addHook("afterCreate",userfollower=>{
+      Activity.current_user(yield userfollower.follower())
+      yield Activity.create({
+        action_type:"created_follower on user_".concat(userfollower.user().name),
+        at:userfollower.follower().created_at,
+        callback_url:use("Route").route("UserFollowerController.show",{follid:productcomment.commid})
+       })
+    })
+  }
+
+  
+   follower(){
+    return this.belongsTo("App/Model/Follower","follid","follid")
+  }
+  user(){
+    return this.belongsTo("App/Model/User","uid","uid")
+  }
+  
+}
+
+module.exports = UserFollower
