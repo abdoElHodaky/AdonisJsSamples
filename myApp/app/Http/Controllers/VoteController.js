38e33d4ebb18c02a60967f5facd1f2e18ab1c1d3
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-19 10:47:26 +0200

    Update VoteController.js

--- myApp/app/Http/Controllers/VoteController.js
+++ myApp/app/Http/Controllers/VoteController.js
@@ -1,1 +1,47 @@
 
+'use strict'
+const Device=use("App/Model/Vote"),
+Product=use("App/Model/Product"),
+User=use("App/Model/User")
+class VoteController {
+   
+  * index(request, response) {
+     response.json(yield user.devices().load("verifications"))
+   }
+
+  * create(request, response) {
+    //
+  }
+
+  * store(request, response) {
+    //
+   var user=request.author.getUser(),
+   inputs=request.post(),
+   entity={}
+   if("product" in inputs)
+     Product
+   
+  // device=yield user.devices().create(inputs)
+   //response.json(device)
+   
+    
+  }
+
+  * show(request, response) {
+    //
+    }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+  }
+
+  * destroy(request, response) {
+    //
+  }
+}
+
+module.exports = VoteController
