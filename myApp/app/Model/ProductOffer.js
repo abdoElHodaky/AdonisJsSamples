5f71487de87732bd1a7b30edcc3e92703bebf47b
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-19 12:21:17 +0200

    Update ProductOffer.js

--- myApp/app/Model/ProductOffer.js
+++ myApp/app/Model/ProductOffer.js
@@ -1,13 +1,26 @@
 'use strict'
 
-const Lucid = use('Lucid')
+const Lucid = use('Lucid'),
+Activity=use("App/Model/Activity")
 
 class ProductOffer extends Lucid {
 
   static get connection () {
     return 'mysql'
   }
-   
+   static boot(){
+    super.boot()
+    this.addHook("afterCreate",productoffer=>{
+      Activity.current_user(yield productoffer.offer().user())
+      yield Activity.create({
+        action_type:"created_offer on job_".concat(productoffer.pid),
+        at:productoffer.offer().created_at,
+        callback_url:use("Route").route("ProductOfferController.show",{offid:offers.offid})
+       })
+    })
+  }
+
+  
    offer(){
     return this.belongsTo("App/Model/Offer","offid","offid")
   }
