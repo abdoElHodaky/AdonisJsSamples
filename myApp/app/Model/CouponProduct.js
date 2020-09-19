af0cc45119cb81454395d18f2fff5d9fe13d30cf
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-19 12:40:19 +0200

    Create CouponProduct.js

---
+++ myApp/app/Model/CouponProduct.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Lucid = use('Lucid'),
+Activity=use("App/Model/Activity")
+
+class CouponProduct extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+   static boot(){
+    super.boot()
+    this.addHook("afterCreate",couponproduct=>{
+      Activity.current_user(yield couponproduct.coupon().user())
+      yield Activity.create({
+        action_type:"created_coupon on product_".concat(couponproduct.product().name),
+        at:productoffer.offer().created_at,
+        callback_url:use("Route").route("CatController.show",
+        {oid:couponproduct.product().order().oid})
+       })
+    })
+  }
+
+  
+   coupon(){
+    return this.belongsTo("App/Model/Coupon","coupid","coupid")
+  }
+  product(){
+    return this.belongsTo("App/Model/Product","pid","pid")
+  }
+  
+}
+
+module.exports = CouponProduct
