2f18501c65a03b26589b2721e9d0d9534d8d75eb
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-19 11:28:00 +0200

    Update Offer.js

--- myApp/app/Model/Offer.js
+++ myApp/app/Model/Offer.js
@@ -3,12 +3,21 @@
 const Lucid = use('Lucid')
 
 class Offer extends Lucid {
-
+  
+ 
+  static castDates (field, value) {
+    if (field.contains("at")==true) {
+      return `${value.fromNow()}`
+    }
+    return super.formatDates(field, value)
+  }
   static get connection () {
     return 'mysql'
   }
    attachments(){
-    return this.manyThrough("App/Model/OfferAttachment","attachments","offid","aid")
+    return this.belongsMany("App/Model/Attachment","pid","aid","aid","pid").
+    pivotModel("App/Model/ProductAttachment")
+    // return this.manyThrough("App/Model/OfferAttachment","attachments","offid","aid")
    }
    user(){
     return this.belongsTo("App/Model/User","uid","uid")
