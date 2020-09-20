ae0f1b2a5b581f5cd74211cb5543a3df64c6224e
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-20 21:14:29 +0200

    Create 1525527617000_create_locations_table.js

---
+++ myApp/database/migrations/1525527617000_create_locations_table.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class LocationsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('locations', (table) => {
+      table.increments("locatid")
+      table.timestamps()
+      table.float("latitude",10,6)
+      table.float("longitude",10,6)
+      table.integer("uid").unique()
+      table.foreign("uid").references("uid").on("users")
+      table.integer("parent_id")
+      table.foreign("parent_id").references("skillid").on("skills")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('locations')
+  }
+
+}
+
+module.exports = LocationsTableSchema
