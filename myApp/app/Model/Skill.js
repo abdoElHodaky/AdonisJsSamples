9d38ec4ecca10e09d88ba9974c742f693a1e2b73
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-20 17:05:31 +0200

    Update Skill.js

--- myApp/app/Model/Skill.js
+++ myApp/app/Model/Skill.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const Lucid = use('Lucid')
+const Lucid = use('Lucid'),
+Activity=use("App/Model/Activity")
 
 class Skill extends Lucid {
   
@@ -10,12 +11,23 @@ class Skill extends Lucid {
   
   static boot(){
    super.boot()
-   
+   this.addHook("afterCreate",skill=>{
+    Activity.current_user(skill.suggested_by)
+    yield Activity.create({
+      action_type:"created_skill"
+      at:product.created_at,
+      //callback_url:use("Route").route("ProductController.show",{pid:product.pid})
+    })
+    
+   })
   }
 
   users(){
     return this.belongsMany("App/Model/User","skillid","uid","uid","skillid").
     pivotTable("App/Model/UserSkill")
+  }
+  suggested_by(){
+    return this.belongsTo("App/Model/User","suggested_by","uid")
   }
 }
 module.exports = Skill
