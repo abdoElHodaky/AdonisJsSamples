760ef22a5d310df3a69a1ef4b35144fcdca5cae1
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-20 12:10:06 +0200

    Update User.js

--- myApp/app/Model/User.js
+++ myApp/app/Model/User.js
@@ -60,7 +60,7 @@ class User extends Lucid {
   }
   events(status=""){
     if (status =="")return this.hasMany("App/Model/Event","uid","uid");
-    else return this.manyThrough("App/Model/EventUser","event","uid","evtid").where({"status":status})
+    else return this.manyThrough("App/Model/EventUser","event","uid","evtid").where({"events_users.status":status})
   }
   users(followers=false){
    return (flowers==false)?this.hasMany("App/Model/User","uid","related_id"):
