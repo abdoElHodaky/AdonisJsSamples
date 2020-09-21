19597740e9d09dab53a3acdea63de2ef31c36557
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-21 19:36:32 +0200

    Update Order.js

--- myApp/app/Model/Order.js
+++ myApp/app/Model/Order.js
@@ -16,19 +16,14 @@ class Order extends Lucid {
     if(order.user().related_id!=0) {
       order.user().commission=.25
       yield order.user().save()
-      order.addHook("afterUpdate",_order=>{
-       //credits_values=yield use("App/Model/OrderedProduct").credits_value
-       if(_order.total!=0)
+      if(order.total!=0)
         {
-          //total=credits_values.reduce((t,v)=>t+=v)
-          yield _order.user().credits().create({
-          value:_order.user().commission*_order.total,
+          yield order.user().credits().create({
+          value:order.user().commission*order.total,
           code:RandomCode(8)
           })
        }
-
-       })
-       }
+     }
     })
    
   }
