6849cf1e6d42b15c3d4e682a3f1fe96ce68ba658
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-21 19:17:06 +0200

    Update AdVisit.js

--- myApp/app/Model/AdVisit.js
+++ myApp/app/Model/AdVisit.js
@@ -4,6 +4,16 @@ const Lucid = use('Lucid')
 
 class AdVisit extends Lucid {
 
+  static get table(){
+   return "visits_on_ads"
+  }
+  static boot(){
+   super.boot()
+  /* this.addHook("afterCreate",advis=>{
+       advis.ad().visits_count++
+       advis.ad.save()
+    })*/
+  }
   static get connection () {
     return 'mysql'
   }
