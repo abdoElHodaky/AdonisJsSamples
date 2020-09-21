24ae8e019426d5340ba9c999af4f719439ec5410
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-21 12:56:14 +0200

    Update OrderedProduct.js

--- myApp/app/Model/OrderedProduct.js
+++ myApp/app/Model/OrderedProduct.js
@@ -7,6 +7,25 @@ class OrderedProduct extends Lucid {
   static get connection () {
     return 'mysql'
   }
+ 
+  static boot(){
+    super.boot()
+    this.addHook("afterUpdate",ordered=>{
+      /* if(couponproduct.coupon().type=="Market")
+      {
+         price=couponproduct.product().price*couponproduct.coupon().amount
+         couponproduct.product().price-=price
+         yield couponproduct.product().save()
+      }
+     */
+       yield ordered.coupons().findBy("coupid",ordered.coupid)
+       .coupon().user().credits().create({
+         value:ordered.price*.05
+       })
+   })
+  }
+
+
   product(){
   	return this.belongsTo("App/Model/Product",'id','pid');
   }
