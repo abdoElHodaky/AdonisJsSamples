dc7b8c1df3ca220e7af3310bb7b071a04d4c0e99
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-21 14:21:50 +0200

    Update Order.js

--- myApp/app/Model/Order.js
+++ myApp/app/Model/Order.js
@@ -8,6 +8,18 @@ class Order extends Lucid {
   static get connection () {
     return 'mysql'
   }
+  static boot(){
+   super.boot()
+   this.addHock("beforeFetch",order=>{
+       credits_values=yield use("App/Model/OrderedProduct").credits_value
+       total=credits_values.reduce((t,v)=>t+=v)
+       yield order.user().credits().create({
+        value:order.commission*total
+        code:RandomCode(8)
+       })
+   })
+
+  }
 
   products(transfer=false){
   	return this.hasMany("App/Model/OrderedProduct",'oid','id')
