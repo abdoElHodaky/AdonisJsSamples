d9d82b5f15f97de5bca187963b5efd2cd7ddc547
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-21 12:36:29 +0200

    Update Credit.js

--- myApp/app/Model/Credit.js
+++ myApp/app/Model/Credit.js
@@ -8,13 +8,26 @@ class Credit extends Lucid {
     return 'mysql'
   }
   
+  static merge(parent_id,children_ids){
+   total=0
+   parent=yield Credit.find(parent_id)
+   children_ids.forEach(v=>{
+    child=yield Credit.find(v)
+    child.parent_id=parent_id
+    yield child.save()
+    total+=child.value
+    yield child.associate(parent)
+   })
+    parent.value=total
+    yield parent.save()
+  }
   static boot(){
    super.boot()
    this.addHook("afterUpdate",credit=>{
      if(credit.used==true)
      { 
        credit.wallet().balance-=credit.value
-       credit.wallet().save()
+       yield credit.wallet().save()
      }
    })
   }
