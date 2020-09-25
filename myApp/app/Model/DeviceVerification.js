128cd86611a7dc8f1f5145e3ef8ddc8e0abcffb3
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-09-25 11:57:52 +0200

    Update DeviceVerification.js

--- myApp/app/Model/DeviceVerification.js
+++ myApp/app/Model/DeviceVerification.js
@@ -1,1 +1,20 @@
 
+'use strict'
+
+const Lucid = use('Lucid')
+
+class DeviceVerification extends Lucid {
+
+  static get connection () {
+    return 'mysql'
+  }
+   verification(){
+    return this.belongsTo("App/Model/Verification","verifid","verifid")
+   }
+   device(){
+    return this.belongsTo("App/Model/Device","devid","devid")
+   }
+
+}
+
+module.exports = DeviceVerification
