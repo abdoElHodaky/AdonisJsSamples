9a0470b1c348b09ab738304e2233bdb2a552dcaa
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2020-10-08 16:21:55 +0200

    Create create_users_followers_table.js

---
+++ myApp/database/migration/create_users_followers_table.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class UsersFollowersTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('users_followers', (table) => {
+      table.increments("follsid")
+      table.timestamps()
+      table.integer("follid").unsigned()
+      table.foreign("follid").references("follid").on("followers")
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('users_followers')
+  }
+
+}
+
+module.exports = UsersFollowersTableSchema
