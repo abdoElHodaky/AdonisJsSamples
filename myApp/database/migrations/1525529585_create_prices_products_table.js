14472a05dcc6ae8d00333d2115524f45b059b195
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2021-05-02 00:43:03 +0200

    Update 1525529585_create_prices_products_table.js

--- myApp/database/migrations/1525529585_create_prices_products_table.js
+++ myApp/database/migrations/1525529585_create_prices_products_table.js
@@ -1,1 +1,33 @@
+'use strict'
 
+const Schema = use('Schema')
+
+class PricesProductsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('prices_products', (table) => {
+      table.increments("pricpid")
+      table.timestamps()
+      table.string("password",50)
+      table.string("hash",100)
+      table.integer("pid").unique()
+      table.foreign("pid").references("pid").on("products)
+      table.integer("pricid").unique()
+      table.foreign("pricid").references("pricid").on("prices)
+      table.integer("parent_id")
+      table.foreign("parent_id").references("pricpid").on("pricpid")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('prices_products')
+  }
+
+}
+
+module.exports = PricesProductsTableSchema
