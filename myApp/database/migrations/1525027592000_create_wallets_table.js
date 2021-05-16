0225c2251999022d41c93590fca4a4f3f585d8fb
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2021-05-16 14:54:01 +0200

    Create 1525027592000_create_wallets_table.js

---
+++ myApp/database/migrations/1525027592000_create_wallets_table.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class WalletsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('wallets', (table) => {
+      table.increments("Wallis")
+      table.timestamps()
+      table.string("address",10)
+      table.boolean("used").default(false)
+      //table.integer("cvv")
+      table.integer("uid").unsigned()
+      table.foreign("uid").references("uid").on("users")
+      table.integer("balance").unsigned()
+      //table.integer("parent_id").unsigned()
+     // table.foreign("parent_id").references("credid").on("credits")
+     // table.integer("wallid").unsigned()
+     // table.foreign("wallid").references("wallid").on("wallets")
+      
+    })
+  }
+
+  down () {
+    this.drop('Wallet')
+  }
+
+}
+
+module.exports = WalletsTableSchema
