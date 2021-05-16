f41a330112be8323c37119c2d7d3ddabf6ed3f71
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2021-05-16 12:36:26 +0200

    Create 1525529645000_create_equity_shares_table.js

---
+++ myApp/database/migrations/1525529645000_create_equity_shares_table.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class EquitySharesTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('equity_shares', (table) => {
+      table.increments("equishid")
+      table.timestamps()
+      table.integer("uid").unique()
+      table.foreign("uid").references("uid").on("users")
+      table.integer("amount")
+      table.float("percentage")
+      table.integer("parent_id")
+      table.foreign("parent_id").references("equishid").on("equishid")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('equity_shares')
+  }
+
+}
+
+module.exports = EquitySharesTableSchema
