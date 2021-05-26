dab8e20bb109932a65b9a4864dc46c94f9b6a897
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2021-05-26 11:59:19 +0200

    Create 1525529820000_create_shops_cats_products_table.js

---
+++ myApp/database/migrations/1525529820000_create_shops_cats_products_table.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ShopsCatsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('cats_shops', (table) => {
+      table.increments("cshid")
+      table.timestamps()
+      table.integer("sid").unsigned()
+      table.foreign("sid").references('sid').on('shops')
+      table.integer("cid").usigned().default(0)
+      table.foreign("cid").references("cid").on("cats")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('cats_shops')
+  }
+
+}
+
+class ProductsCatsTableSchema extends Schema {
+
+  static get connection () {
+    return 'mysql'
+  }
+
+  up () {
+    this.create('products_cats', (table) => {
+      table.increments("pidcid")
+      table.timestamps()
+      table.integer("pid").unsigned()
+      table.foreign("pid").references('pid').on('products')
+      table.integer("cid").usigned().default(0)
+      table.foreign("cid").references("cid").on("cats","cats_shops")
+      table.softDeletes()
+    })
+  }
+
+  down () {
+    this.drop('products_cats')
+  }
+
+}
+
+module.exports = {CatsTableSchema,ProductsCatsTableSchema}
