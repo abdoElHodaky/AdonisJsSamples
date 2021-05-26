3228f75bae4860eb27613699648e51dfeda46205
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2021-05-26 15:05:46 +0200

    Update 1525027532417_create_credit_table.js

--- myApp/database/migrations/1525027532417_create_credit_table.js
+++ myApp/database/migrations/1525027532417_create_credit_table.js
@@ -15,13 +15,14 @@ class CreditsTableSchema extends Schema {
       table.string("credit_No",10)
       table.boolean("used").default(false)
       //table.integer("cvv")
-     // table.integer("uid").unsigned()
-     // table.foreign("uid").references("uid").on("users")
+       table.integer("uid").unsigned()
+       table.foreign("uid").references("uid").on("users")
       table.integer("value").unsigned().default(500)
       table.integer("parent_id").unsigned()
       table.foreign("parent_id").references("credid").on("credits")
       table.integer("wallid").unsigned()
       table.foreign("wallid").references("wallid").on("wallets")
+      table.enu("for",["transaction","businesses"])
       
     })
   }
