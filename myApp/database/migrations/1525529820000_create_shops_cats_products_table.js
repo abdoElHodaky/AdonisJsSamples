54ebdc40177513c7f9d91f8176d7fe8d85b95309
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2021-05-26 13:48:42 +0200

    Update 1525529820000_create_shops_cats_products_table.js

--- myApp/database/migrations/1525529820000_create_shops_cats_products_table.js
+++ myApp/database/migrations/1525529820000_create_shops_cats_products_table.js
@@ -26,29 +26,6 @@ class ShopsCatsTableSchema extends Schema {
 
 }
 
-class ProductsCatsTableSchema extends Schema {
 
-  static get connection () {
-    return 'mysql'
-  }
 
-  up () {
-    this.create('products_cats', (table) => {
-      table.increments("pidcid")
-      table.timestamps()
-      table.integer("pid").unsigned()
-      table.foreign("pid").references('pid').on('products')
-      table.integer("cid").usigned().default(0)
-      table.foreign("cid").references("cid").on("cats")
-      table.foreign("cid").references("cid").on("cats_shops")
-      table.softDeletes()
-    })
-  }
-
-  down () {
-    this.drop('products_cats')
-  }
-
-}
-
-module.exports = [CatsTableSchema,ProductsCatsTableSchema]
+module.exports = CatsTableSchema
